ffa8f8080a34e70c1f6e705467fd004e22249c8c
Dhanya122 <dhanya.a@intel.com>
2025-06-13 13:04:21 +0000

    change procedure to compress folders

--- idv/setup_rpm_source.sh
+++ idv/setup_rpm_source.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo cp autologin.conf $HOME/rpmbuild/SOURCES
-sudo cp etc/systemd/user/* $HOME/rpmbuild/SOURCES
 sudo cp setup_permissions.sh $HOME/rpmbuild/SOURCES
 sudo cp idv-solution-1.0.tar.gz $HOME/rpmbuild/SOURCES
 
