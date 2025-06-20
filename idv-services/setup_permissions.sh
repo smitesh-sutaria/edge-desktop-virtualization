11acda3097431341592fa08a64b5d4e71cde7b31
Dhanya122 <dhanya.a@intel.com>
2025-06-20 11:19:36 +0530

    Update file

--- idv-services/setup_permissions.sh
+++ idv-services/setup_permissions.sh
@@ -3,4 +3,5 @@
 # Copyright (C) 2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-echo "ALL ALL=(ALL) NOPASSWD: /usr/bin/X,/opt/idv/init/setup_sriov_vfs.sh,/opt/idv/init/setup_display.sh,/opt/idv/init/setup_file_permissions.sh,/opt/idv/launcher/start_vm.sh,/opt/idv/launcher/start_all_vms.sh,/opt/idv/launcher/stop_vm.sh,/opt/idv/launcher/stop_all_vms.sh" | sudo tee -a /etc/sudoers.d/guest > /dev/null
+echo "ALL ALL=(ALL) NOPASSWD: /usr/bin/X,/usr/local/bin/idv/init/setup_sriov_vfs.sh,/usr/local/bin/idv/init/setup_display.sh,/usr/local/bin/idv/init/setup_file_permissions.sh,/usr/local/bin/idv/launcher/start_vm.sh,/usr/local/bin/idv/launcher/start_all_vms.sh,/usr/local/bin/idv/launcher/stop_vm.sh,/usr/local/bin/idv/launcher/stop_all_vms.sh" | sudo tee -a /etc/sudoers.d/guest > /dev/null
+
