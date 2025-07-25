2d772475fe569908d35a2c54f602f9589211da5e
Smitesh Sutaria <smitesh.sutaria@intel.com>
2025-07-25 13:56:07 +0530

    creating build function, to modularize operations
    Signed-off-by: Smitesh Sutaria <smitesh.sutaria@intel.com>

--- emt-dv-iso/build_idv_iso.sh
+++ emt-dv-iso/build_idv_iso.sh
@@ -5,40 +5,44 @@
 
 GIT_REPO=https://github.com/open-edge-platform/edge-microvisor-toolkit.git
 DEFAULT_TAG=3.0.20250718
-IDV_JSON_PATH=idv.json
+IDV_JSON_PATH=""
 
 TAG=${1:-$DEFAULT_TAG}
 
-# clone the emt repo
-git clone $GIT_REPO
-cd edge-microvisor-toolkit
-# checkout the required TAG
-git checkout $TAG
+function launch_build() {
+    # clone the emt repo
+    git clone $GIT_REPO
+    cd edge-microvisor-toolkit
+    # checkout the required TAG
+    git checkout $TAG
 
-# pre-requisites
-sudo ./toolkit/docs/building/prerequisites-ubuntu.sh
-sudo ln -vsf /usr/lib/go-1.21/bin/go /usr/bin/go
-sudo ln -vsf /usr/lib/go-1.21/bin/gofmt /usr/bin/gofmt
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo usermod -aG docker $USER
+    # pre-requisites
+    sudo ./toolkit/docs/building/prerequisites-ubuntu.sh
+    sudo ln -vsf /usr/lib/go-1.21/bin/go /usr/bin/go
+    sudo ln -vsf /usr/lib/go-1.21/bin/gofmt /usr/bin/gofmt
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo usermod -aG docker $USER
 
-# build the toolkit
-cd toolkit
-if [[ -z "$IDV_JSON_PATH" ]]; then
-    wget https://raw.githubusercontent.com/open-edge-platform/edge-desktop-virtualization/refs/heads/emt-dv-iso/emt-dv-iso/idv.json
-    cp idv.json ./imageconfigs
-else
-    cp $IDV_JSON_PATH ./imageconfigs/idv.json
+    # build the toolkit
+    cd toolkit
+    if [[ -z "$IDV_JSON_PATH" ]]; then
+        wget https://raw.githubusercontent.com/open-edge-platform/edge-desktop-virtualization/refs/heads/emt-dv-iso/emt-dv-iso/idv.json
+        cp idv.json ./imageconfigs
+    else
+        cp $IDV_JSON_PATH ./imageconfigs/idv.json
 
-sudo make toolchain REBUILD_TOOLS=y VALIDATE_TOOLCHAIN_GPG=n
+    sudo make toolchain REBUILD_TOOLS=y VALIDATE_TOOLCHAIN_GPG=n
 
-# build the iso image
-sudo make iso -j8 REBUILD_TOOLS=y REBUILD_PACKAGES=n VALIDATE_TOOLCHAIN_GPG=n CONFIG_FILE=./imageconfigs/idv.json
+    # build the iso image
+    sudo make iso -j8 REBUILD_TOOLS=y REBUILD_PACKAGES=n VALIDATE_TOOLCHAIN_GPG=n CONFIG_FILE=./imageconfigs/idv.json
 
-# copy the generated iso to same parent folder
-cp ../out/images/idv/*.iso ../../.
+    # copy the generated iso to same parent folder
+    cp ../out/images/idv/*.iso ../../.
 
-# cleanup
-cd ../../
-sudo rm -rf edge-microvisor-toolkit
+    # cleanup
+    cd ../../
+    sudo rm -rf edge-microvisor-toolkit
+}
+
+launch_build
